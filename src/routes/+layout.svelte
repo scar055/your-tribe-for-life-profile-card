b38d2842fcfb7a8b77c7cdd5236a295fd45b383e
Rowan <48519289+scar055@users.noreply.github.com>
2026-09-17 14:08:48 +0200

    voeg een background toe #3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import favicon from "$lib/assets/favicon.svg";
+	import "../app.css";
 
 	let { children } = $props();
 </script>
@@ -18,5 +19,12 @@
 		height: 100dvh;
 		justify-content: center;
 		align-items: center;
+
+		background: #99ffaa;
+		background: linear-gradient(
+			135deg,
+			rgba(153, 255, 170, 1) 0%,
+			rgba(114, 242, 242, 1) 100%
+		);
 	}
 </style>
