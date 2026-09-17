bdf7d219eb9181f09a3c99a25bd44c0f8f138dd7
Rowan <48519289+scar055@users.noreply.github.com>
2026-09-17 14:08:35 +0200

    verander directus link #3

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,6 @@
 export async function load({ fetch }) {
 	const res = await fetch(
-		`https://fdnd.directus.app/items/person/?filter[squads][squad_id][name]=2G&fields=id,name,fav_game,vibe_emoji,residency,fav_hobby,fav_color,bio,website,github_handle,nickname,mugshot,birthdate&filter[id][_eq]=249`,
+		`https://fdnd.directus.app/items/person?fields=*&filter[squads][squad_id][cohort][_eq]=2627&sort=name&filter[id][_eq]=249`,
 	);
 	const data = await res.json();
 
